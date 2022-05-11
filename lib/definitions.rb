8a0541b61d517a3991bbcea1808fed79cfef31c6
Kyle Miller <millerkyle85@gmail.com>
2022-05-11 16:59:20 -0700

    fix errors

--- lib/definitions.rb
+++ lib/definitions.rb
@@ -1,50 +1,45 @@
 class Definitions
   attr_reader :id 
-  attr_accessor  :wordid, :definition
+  attr_accessor  :word_id, :definition
 
   @@definitions = {}
   @@total_rows = 0
 
-  def initalize(new_definition, word_id, id)
+  def initialize(new_definition, word_id, id)
     @definition = new_definition
-    @wordid = word_id
+    @word_id = word_id
     @id = id || @@total_rows += 1
   end
 
   def ==(def_to_compare)
-    (self.new_definition() == def_to_compare.new_definition()) && (self.word_id() == def_to_compare.word_id())
+    (self.definition() == def_to_compare.definition()) && (self.word_id() == def_to_compare.word_id())
   end
 
   def self.all
     @@definitions.values
   end
 
-  def save 
-    @@definitions[self.id] = Definitions.new(self.definition, self.wordid)
-  end
-
-  def ==(another_definition)
-    self.definition() == another_definition.definition()
+  def save() 
+    @@definitions[self.id] = Definitions.new(self.definition, self.word_id, self.id)
   end
 
   def self.clear
     @@definitions = {}
-    @@total_rows = 0
   end
 
   def self.find(id)
     @@definitions[id]
   end
 
-  def update(definition)
-    @definitions = definition
+  def update(new_definition)
+    @definition = new_definition
   end
   
   def delete
     @@definitions.delete(self.id)
   end
 
-  def self.find_by_wordid(word_id)
+  def self.find_by_word(word_id)
     definitions = []
     @@definitions.values.each do |definition|
       if definition.word_id == word_id
@@ -53,4 +48,10 @@ class Definitions
     end
     definitions
   end
+ 
+
+
+  # def self.find_by_word(word_id)
+  #   Word.find(word_id)
+  # end
 end
